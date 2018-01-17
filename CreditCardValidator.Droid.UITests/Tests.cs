1ff8934a72c365ccb75aec940950f1ee700c4ee2
Marek Urban <marek.urban@pl.abb.com>
2018-01-17 23:15:53 +0100

    all files

---
+++ CreditCardValidator.Droid.UITests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace CreditCardValidator.Droid.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+		}
+
+	}
+}
+
